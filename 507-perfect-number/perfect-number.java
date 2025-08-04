2971f946f27776aa24138504c80439ea22b2d709
Anshu Vairagade <143610614+AnshuVairagade@users.noreply.github.com>
2025-08-04 16:45:13 +0530

    Time: 0 ms (100.00%) | Memory: 40.7 MB (29.55%) - LeetSync

---
+++ 507-perfect-number/perfect-number.java
@@ -0,0 +1,7 @@
+class Solution {
+    public boolean checkPerfectNumber(int num) {
+        return (num == 6 || num == 28 || num == 496 || num == 8128 || num == 33550336) ? true : false;
+    }
+}
+
+// There exist only 5 perfect number in integer range
