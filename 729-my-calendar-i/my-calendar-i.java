08ec432358d9be40723bbd378fe797336d688c44
Anshu Vairagade <143610614+AnshuVairagade@users.noreply.github.com>
2026-01-16 12:33:48 +0530

    Time: 86 ms (19.89%) | Memory: 47.1 MB (88.16%) - LeetSync

---
+++ 729-my-calendar-i/my-calendar-i.java
@@ -0,0 +1,25 @@
+class MyCalendar {
+    List<int[]> list;
+
+    public MyCalendar() {
+        list = new ArrayList<>();
+    }
+    
+    public boolean book(int startTime, int endTime) {
+
+        for(int[] event : list){
+            if(startTime < event[1] && endTime > event[0]){
+                return false;
+            }
+        }
+
+        list.add(new int[]{startTime, endTime});
+        return true;
+    }
+}
+
+/**
+ * Your MyCalendar object will be instantiated and called as such:
+ * MyCalendar obj = new MyCalendar();
+ * boolean param_1 = obj.book(startTime,endTime);
+ */
