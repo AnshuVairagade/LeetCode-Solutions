31ae385336abf1aa5f1e1f84be668c086adcf3df
Anshu Vairagade <143610614+AnshuVairagade@users.noreply.github.com>
2026-01-29 20:40:23 +0530

    Time: 3 ms (88.58%) | Memory: 47.9 MB (27.65%) - LeetSync

--- 135-candy/candy.java
+++ 135-candy/candy.java
@@ -1,83 +1,27 @@
 class Solution {
-    public int candy(int[] ratings) {
-        int n = ratings.length;
-        HashMap<Integer, List<Integer>> map = new HashMap<>();
-
-        int maxRating = 0;
-        for(int i=0; i<ratings.length; i++){
-            List<Integer> list = map.getOrDefault(ratings[i], new ArrayList<>());
-            list.add(i);
-            map.put(ratings[i], list);
-
-            maxRating = Math.max(maxRating, ratings[i]);
+    public int candy(int[] rating) {
+        int n = rating.length;
+        int[] count = new int[n];
+        
+        for(int i = 0; i< n ; i++){
+            count[i]=1;
         }
-
-        int[] candies = new int[n];
-        for(int rating=0; rating<=maxRating; rating++){
-            if(!map.containsKey(rating)) continue;
-            
-            List<Integer> list = map.get(rating);
-            for(int i : list){
-                int pr = i>0 ? ratings[i-1] : 0;
-                int nr = i+1<n ? ratings[i+1] : 0;
-                int pc = i>0 ? candies[i-1] : 0;
-                int nc = i+1<n ? candies[i+1] : 0;
-                int cr = ratings[i];
-
-                if(pr<cr && cr>nr){
-                    candies[i] = Math.max(pc, nc) + 1;
-                }
-                else if(pr < cr){
-                    candies[i] = pc + 1;
-                }
-                else if(nr < cr){
-                    candies[i] = nc + 1;
-                }
-                else candies[i] =  1;
+        for(int i = 1; i < n;i++){
+            if(rating[i]>rating[i-1]){
+                count[i]=count[i-1]+1;
             }
         }
-
-        int totalCandies = 0;
-        for(int cnt : candies) totalCandies += cnt;
-
-        return totalCandies;
+        for(int i = n-2; i>=0;i--){
+            if(rating[i]>rating[i+1]){
+                count[i]=Math.max(count[i],count[i+1]+1);
+            }
+        }
+        
+        int ans = 0; 
+        for(int i = 0; i< n ;i++){
+            ans+=count[i];
+        }
+        
+        return ans;
     }
 }
-
-/*
-
-Approach : 
-1) Distribute candies from lower rating to higher rating
-2) To assign candies check previous and next rating conditions
-
-
-TC = O( n + max)
-SC = O(n + n)
-
-Edge Cases:
-
-Test case 1
-[1,2,87,4,3,2,1]
-
-
-Test case 2
-[1,2,87,87,87,2,1]
-
-
-Test case 3
-[1,2,3,1,3,2,1]
-
-
-Test case 4
-[1,2,5,4,3,2,1]
-
-
-Test case 5
-[1,2,4,4,3]
-
-
-Test case 6
-[1,2,5,4,3,2]
-
-
- */
