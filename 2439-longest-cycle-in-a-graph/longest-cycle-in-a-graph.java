90ef5ff21aff1cce8894c04637e6a369316298ba
Anshu Vairagade <143610614+AnshuVairagade@users.noreply.github.com>
2026-02-09 14:26:19 +0530

    Time: 112 ms (24.77%) | Memory: 169 MB (17.37%) - LeetSync

--- 2439-longest-cycle-in-a-graph/longest-cycle-in-a-graph.java
+++ 2439-longest-cycle-in-a-graph/longest-cycle-in-a-graph.java
@@ -1,72 +1,66 @@
-// Intuition : Famous Algorithm for SCC
+// Hint  : by using Topo Sort we can islolate the cycles
 
 class Solution {
-    Stack<Integer> st = new Stack<>();
-
     public int longestCycle(int[] edges) {
-        List<List<Integer>> graph = new ArrayList<>();
-        List<List<Integer>> revGraph = new ArrayList<>();
         int n = edges.length;
 
-        for(int i=0; i<n; i++){
-            graph.add(new ArrayList<>());
-            revGraph.add(new ArrayList<>());
-        }
+        HashMap<Integer, Integer> graph = new HashMap<>();
+        int indegree[] = new int[n];
 
         for(int i=0; i<n; i++){
             if(edges[i] == -1) continue;
 
-            graph.get(i).add(edges[i]);
-            revGraph.get(edges[i]).add(i);
+            indegree[edges[i]]++;
+            graph.putIfAbsent(i, edges[i]);
+        }
+
+        Queue<Integer> queue = new LinkedList<>();
+        for(int i=0; i<n; i++){
+            if(indegree[i] == 0){
+                queue.offer(i);
+            }
+        }
+
+        while(!queue.isEmpty()){
+            int node = queue.poll();
+
+            int adjNode = graph.getOrDefault(node, -1);
+            if(adjNode == -1) continue;
+            indegree[adjNode]--;
+                
+            if(indegree[adjNode] == 0){
+                queue.offer(adjNode);
+            }
         }
 
         int vis[] = new int[n];
-        for(int i=0; i<n; i++){
-            if(vis[i] == 1) continue;
-            normal_dfs(i, graph, vis);
-        }
-
         int longestCycle = -1;
-        int vis2[] = new int[n];
-        while(!st.empty()){
-            int node = st.peek(); st.pop();
-            
-            if(vis2[node] == 1) continue;
-            
-            int cycle = time_based_dfs(node, revGraph, vis2);
-            longestCycle = Math.max(cycle, longestCycle);
+
+        for(int i=0; i<n; i++){
+            if(indegree[i] == 0 || vis[i] == 1) continue;
+
+            int cycle = normal_dfs(i, graph, vis);
+            longestCycle = Math.max(longestCycle, cycle);
         }
 
-        return longestCycle == 1 ? -1 : longestCycle;
+        return longestCycle;
     }
 
-    private int time_based_dfs(int node, List<List<Integer>> revGraph, int[] vis2){
-        if(vis2[node] == 1){
+
+    private int normal_dfs(int node, HashMap<Integer, Integer> graph, int[] vis){
+        if(vis[node] == 1){
             return 0;
         }
 
-        vis2[node] = 1;
-
-        int cnt = 1;
-        for(int adjNode : revGraph.get(node)){
-            cnt += time_based_dfs(adjNode, revGraph, vis2);
-        }
-
-        return cnt;
-    }
-
-    private void normal_dfs(int node, List<List<Integer>> graph, int[] vis){
-        if(vis[node] == 1){
-            return;
-        }
-
         vis[node] = 1;
+        int adjNode = graph.get(node);
 
-        for(int adjNode : graph.get(node)){
-            normal_dfs(adjNode, graph, vis);
-        }
+        // int cnt = 1;
+        // for(int adjNode : graph.get(node)){
+        //     normal_dfs(adjNode, graph, vis);
+        // }
 
-        st.push(node);
+        return 1 + normal_dfs(adjNode, graph, vis);
     }
 
 
