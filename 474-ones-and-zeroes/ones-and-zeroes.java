4d94ae7176cdfd01fa528c4b5ca34e7ecf7211e1
Anshu Vairagade <143610614+AnshuVairagade@users.noreply.github.com>
2026-02-09 21:05:14 +0530

    Time: 55 ms (33.37%) | Memory: 86.5 MB (26.08%) - LeetSync

--- 474-ones-and-zeroes/ones-and-zeroes.java
+++ 474-ones-and-zeroes/ones-and-zeroes.java
@@ -1,85 +1,33 @@
 class Solution {
     public int findMaxForm(String[] strs, int m, int n) {
-        int dp[][][] = new int[strs.length][m+1][n+1];
-        for(int i=0; i<strs.length; i++){
+        int len = strs.length;
+        int dp[][][] = new int[len+1][m+1][n+1];
+        
+        for(int idx = len-1; idx>=0; idx--){
+
+            int zeros = 0, ones = 0;
+            for(char dig : strs[idx].toCharArray()){
+                if(dig == '1') ones++;
+                else zeros++;
+            }
+
             for(int j=0; j<=m; j++){
-                for(int k=0; k<=n; k++) dp[i][j][k] = -1;
+                for(int k=0; k<=n; k++){
+
+                    int nopick = dp[idx+1][j][k];
+
+                    int pick = 0;
+                    if(zeros <= j && ones <= k){
+                        pick = 1 + dp[idx+1][j-zeros][k-ones];
+                    }
+
+                    dp[idx][j][k] = Math.max(pick, nopick);
+                }
             }
         }
 
-        int temp = helper(0, m, n, strs, dp);  
-        return temp;
+        return dp[0][m][n];
     }
 
-    private int helper(int idx, int m, int n, String[] strs, int[][][] dp){
-        // Bug point removed
-
-        if(idx == strs.length){
-            return 0;
-        }
-
-        if(dp[idx][m][n] != -1) return dp[idx][m][n];
-
-        int zeros = 0, ones = 0;
-        for(char dig : strs[idx].toCharArray()){
-            if(dig == '1') ones++;
-            else zeros++;
-        }
-
-        int nopick = helper(idx+1, m, n, strs, dp);
-
-        int pick = 0;
-        if(zeros <= m && ones <= n){
-            pick = 1 + helper(idx+1, m-zeros, n-ones, strs, dp);
-        }
-
-        return dp[idx][m][n] = Math.max(pick, nopick);
-    }
 }
 
-
-/*
-
-
-class Solution {
-    List<List<String>> res = new ArrayList<>();
-    
-    public int findMaxForm(String[] strs, int m, int n) {
-        List<String> list = new ArrayList<>();
-        int temp = helper(0, m, n, strs, list);        
-        System.out.println(res);
-        return temp;
-    }
-
-    private int helper(int idx, int m, int n, String[] strs, List<String> list){
-        if(m<=0 || n<=0){
-            res.add(new ArrayList<>(list));
-            return 0;
-        }
-
-        if(idx == strs.length){
-            res.add(new ArrayList<>(list));
-            return 0;
-        }
-
-        int zeros = 0, ones = 0;
-        for(char dig : strs[idx].toCharArray()){
-            if(dig == '1') ones++;
-            else zeros++;
-        }
-
-        int nopick = helper(idx+1, m, n, strs, list);
-
-        int pick = 0;
-        if(zeros <= n && ones <= m){
-            list.add(strs[idx]);
-            pick = 1 + helper(idx+1, m-zeros, n-ones, strs, list);
-            list.remove(list.size()-1);
-        }
-
-        return Math.max(pick, nopick);
-    }
-}
-
-
-*/
