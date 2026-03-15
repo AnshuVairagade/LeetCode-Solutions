e024225e2a4e8fb18f38c5a5b629867c288fd309
Anshu Vairagade <143610614+AnshuVairagade@users.noreply.github.com>
2026-03-15 23:14:06 +0530

    Time: 31 ms (61.32%) | Memory: 124.8 MB (20.57%) - LeetSync

--- 373-find-k-pairs-with-smallest-sums/find-k-pairs-with-smallest-sums.java
+++ 373-find-k-pairs-with-smallest-sums/find-k-pairs-with-smallest-sums.java
@@ -1,44 +1,26 @@
-// Why to use HashMap??
+// How I removed extra space of HashSet/HashMap
 
 class Solution {
     public List<List<Integer>> kSmallestPairs(int[] nums1, int[] nums2, int k) {
-
+        PriorityQueue<Pair> pq = new PriorityQueue<>((a,b) -> a.sum - b.sum);
         List<List<Integer>> res = new ArrayList<>();
 
-        PriorityQueue<Pair> pq = new PriorityQueue<>((a,b) -> a.sum - b.sum);
-
-        HashSet<String> visited = new HashSet<>();
-
-        pq.offer(new Pair(nums1[0] + nums2[0], 0, 0));
-        visited.add("0,0");
-
-        while(k > 0 && !pq.isEmpty()){
-
-            Pair pair = pq.poll();
-            int lp = pair.lp;
-            int rp = pair.rp;
-
-            res.add(Arrays.asList(nums1[lp], nums2[rp]));
-
-            if(lp + 1 < nums1.length){
-                String key = (lp+1) + "," + rp;
-                if(!visited.contains(key)){
-                    pq.offer(new Pair(nums1[lp+1] + nums2[rp], lp+1, rp));
-                    visited.add(key);
-                }
-            }
-
-            if(rp + 1 < nums2.length){
-                String key = lp + "," + (rp+1);
-                if(!visited.contains(key)){
-                    pq.offer(new Pair(nums1[lp] + nums2[rp+1], lp, rp+1));
-                    visited.add(key);
-                }
-            }
-
-            k--;
+        for(int i=0; i<nums1.length; i++){
+            pq.offer(new Pair(nums1[i]+nums2[0], i, 0));
         }
+        
+        while(k-- > 0){
+            Pair pair = pq.poll();
+            int lp = pair.lp, rp = pair.rp;
 
+            List<Integer> temp = Arrays.asList(nums1[lp], nums2[rp]);
+            res.add(temp);
+
+            if(rp+1 < nums2.length){
+                pq.offer(new Pair(nums1[lp]+nums2[rp+1], lp, rp+1));
+            }
+        }
+    
         return res;
     }
 
@@ -47,7 +29,7 @@ class Solution {
         int lp;
         int rp;
 
-        Pair(int sum, int lp, int rp){
+        public Pair(int sum, int lp, int rp){
             this.sum = sum;
             this.lp = lp;
             this.rp = rp;
